dab08bd48df881ce5e7be070fb4dd4724a11cd1b
Karneet Arora <44528134+karneetarora@users.noreply.github.com>
2020-04-26 20:00:25 -0400

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -13,56 +13,51 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
-    RadioGroup radioGroup;
+    RadioGroup rg;
+    RadioButton rb;
     EditText weightText, heightText;
-    int idd, weight, height = 0;
+    int weight, height = 0;
     static double BMI;
     String textBMI;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        textView =findViewById(R.id.textViewBMI);
-        radioGroup = findViewById(R.id.radioGroup);
 
-        weightText = findViewById(R.id.textWeight);
-        heightText = findViewById(R.id.textHeight);
+        if (savedInstanceState != null) {
+            weightText.setText(savedInstanceState.getString("savedWeight"));
+            heightText.setText(savedInstanceState.getString("savedHeight"));
+            textView.setText(savedInstanceState.getString("savedBMI"));
+        }
+    else {
+            setContentView(R.layout.activity_main);
+            textView = findViewById(R.id.textViewBMI);
+            rg = findViewById(R.id.radioGroup);
 
-        radioGroup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                idd = radioGroup.getCheckedRadioButtonId();
-                if(idd == R.id.rbEnglish) {
-                    weightText.setHint("Enter Weight in Pounds");
-                    heightText.setHint("Enter Height in Inches");
-                }
-                else {
-                    weightText.setHint("Enter Weight in Kilograms");
-                    heightText.setHint("Enter Height in Meters");
-                }
+            weightText = findViewById(R.id.textWeight);
+            heightText = findViewById(R.id.textHeight);
 
-            }
-        });
+        }
     }
 
     /**
      * Changes the hints to pounds/inches or kilograms/meters.
      * @param view Selected button in radio group.
      */
-    public void radioClicked(View view) {
-        // change everything to whatever was selected: "hint"
-        // take it into consideration when calculating
+    public void rbClick(View view) {
 
-        if(idd == R.id.rbEnglish) {
-            weightText.setHint("Enter Weight in Pounds");
-            heightText.setHint("Enter Height in Inches");
+        int radiobuttonid = rg.getCheckedRadioButtonId();
+        rb = findViewById(radiobuttonid);
+        // Toast.makeText(getBaseContext(), rb.getText(), Toast.LENGTH_LONG).show();
+        if(R.id.rbEnglish == rb.getId()){
+            weightText.setHint("Enter weight in pounds");
+            heightText.setHint("Enter height in inches");
+        }else{
+            weightText.setHint("Enter weight in kilograms");
+            heightText.setHint("Enter height in meters");
+
         }
-        else {
-            weightText.setHint("Enter Weight in Kilograms");
-            heightText.setHint("Enter Height in Meters");
-        }
+
     }
 
     /**
@@ -70,21 +65,35 @@ public class MainActivity extends AppCompatActivity {
      * @param view Clicked button "CALCULATE BMI"
      */
     public void showBMI(View view) {
-        weight = Integer.valueOf(weightText.getText().toString());
-        height = Integer.valueOf(heightText.getText().toString());
-        if(weight != 0 && height != 0) {
-            if (idd == R.id.rbEnglish)
+        String msg;
+        
+        String weightString = "" + weightText.getText().toString();
+        weightString = weightString.replaceFirst("^0+(?!$)", "");
+
+        String heightString = "" + heightText.getText().toString();
+        heightString = heightString.replaceFirst("^0+(?!$)", "");
+
+        if((weightString.equals("") || weightString.equals("0"))  &&
+                ( heightString.equals("") ||  heightString.equals("0") ) ){
+            msg = "Please enter a weight and height";
+            Toast.makeText(getApplicationContext(),msg, Toast.LENGTH_LONG).show();
+
+        }else if(weightString.equals("") || weightString.equals("0")){
+            msg = "Please enter a weight";
+            Toast.makeText(getApplicationContext(),msg, Toast.LENGTH_LONG).show();
+
+        }else if(heightString.equals("") || heightString.equals("0") ){
+            msg = "Please enter a height";
+            Toast.makeText(getApplicationContext(),msg, Toast.LENGTH_LONG).show();
+        }else{
+            weight = Integer.valueOf(weightText.getText().toString());
+            height = Integer.valueOf(heightText.getText().toString());
+            if (R.id.rbEnglish == rb.getId())
                 BMI = (weight * 703) / (height * height);
             else
                 BMI = (weight) / (height * height);
             textBMI = (String.format("%.2f", BMI));
             textView.setText(textBMI);
-        }
-        else{
-            if(weightText.getText().equals(null) || weight == 0 )
-                Toast.makeText(getApplicationContext(), "Please enter a Weight", Toast.LENGTH_LONG).show();
-            else
-                Toast.makeText(getApplicationContext(), "Please enter a Height", Toast.LENGTH_LONG).show();
         }
     }
 
@@ -101,6 +110,14 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putString("savedWeight", weightText.getText().toString());
+        savedInstanceState.putString("savedHeight", heightText.getText().toString());
+        savedInstanceState.putString("savedBMI", textBMI);
+        super.onSaveInstanceState(savedInstanceState);
+    }
 
 }
 
